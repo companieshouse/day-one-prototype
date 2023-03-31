1096cd533d5d60be45aee234083d3e26c9251ee9
James Francis <jfranciswebdesign@users.noreply.github.com>
2023-03-31 09:20:45 +0100

    Put routing in for test scenarios

--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -2,18 +2,18 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // V1 Test scenario
-router.post('/check-your-answer', function (req, res) {
+router.post('/v1/standalone-service/change-email-address', function (req, res) {
 
-  // Make a variable and give it the value from 'scenario'
+  // Make a variable and give it the value from 'scenario' (test-scenario.html)
   var scenario = req.session.data['scenario']
 
   // If 'verified'
   if (scenario == "verified"){
     // Send user to verification version of Check your answers
-    res.redirect('/verification/check-your-answer')
+    res.redirect('/v1/standalone-service/verification/check-your-answer')
   } else {
     // Send user to normal (unverified) version of Check your answers
-    res.redirect('/check-your-answer')
+    res.redirect('/v1/standalone-service/check-your-answer')
   }
 
 })
