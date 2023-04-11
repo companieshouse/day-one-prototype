c8984bbcf1e8c1eb613a2e8067341477d06bd36a
James Francis <jfranciswebdesign@users.noreply.github.com>
2023-04-11 11:17:04 +0100

    Update routes

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,9 +2,9 @@ module.exports = {
 
   // Insert values here
 
-  "email": [ "user@acmelimited.co.uk" ],
-  "company-number": [ "01234567" ],
-  "new-email": [ "contact@acmelimited.com" ]
+ // "email": [ "user@acmelimited.co.uk" ],
+//  "company-number": [ "01234567" ],
+ // "new-email": [ "contact@acmelimited.com" ]
 
 
 
